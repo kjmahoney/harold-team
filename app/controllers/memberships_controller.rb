b5093eb384de15629dc06326c7ab1b9423e34e0f
Kevin Mahoney <kjmahoney13@gmail.com>
2016-10-19 14:00:56 -0400

    added ability to create memberships

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -3,9 +3,16 @@ class MembershipsController < ApplicationController
     @membership = Membership.new
     @users = User.all
     @teams = Team.all
-
   end
 
   def create
+    @membership =Membership.create!(membership_params)
+    redirect_to teams_path
   end
+
+  private
+  def membership_params
+    params.require(:membership).permit(:user_id, :team_id)
+  end
+
 end
